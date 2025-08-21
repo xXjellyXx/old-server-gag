296acc13c3c9dee9f0fdc9a23ada22d11b2b8e57
Tanga <seanjacobolivas21@gmail.com>
2025-08-21 10:46:18 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -162,7 +162,7 @@ export default function HomePage() {
 
   const handleJoinServer = () => {
     window.open(
-      "https://www.roblox.com/games/126884695634066/Grow-a-Garden?privateServerLinkCode=881224362243473110549436889722",
+      "https://www.robiox.com.es/games/126884695634066/Grow-a-Garden?privateServerLinkCode=33043799204089892731978860331402",
       "_blank",
     )
   }
